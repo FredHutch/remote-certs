c439249281aca28bccf89b6df0064b49e4dc4984
Michael Gutteridge <mrg@fhcrc.org>
2016-05-03 13:27:35 -0700

    added attributes and cookbook information

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,28 @@
 name 'hutch-certs'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
+maintainer 'Scientific Computing'
+maintainer_email 'scicomp@fhcrc.org'
 license 'all_rights'
-description 'Installs/Configures hutch-certs'
-long_description 'Installs/Configures hutch-certs'
+description 'Installs Certificates'
+long_description 'Installs Certificates from HTTP Server for use by OpenSSL'
 version '0.1.0'
+
+attribute 'organization',
+    :display_name => 'Organization Name',
+    :description  => 'The name of the organization that owns the certificate',
+    :type         => 'string',
+    :required     => 'required',
+    :default      => 'organization'
+
+attribute 'cert_source',
+    :display_name => 'URL containing source',
+    :description  => 'URL for the certificate source',
+    :type         => 'string',
+    :required     => 'required'
+
+attribute 'update_command',
+    :display_name => 'CA Update Command',
+    :description  => 'Command used to update CA chain',
+    :type         => 'string',
+    :required     => 'required',
+    :default      => '/usr/sbin/update-ca-certificates -f'
+
