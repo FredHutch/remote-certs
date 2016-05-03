eae0fbb81ac077c4d03d3d6a156e9d8dc470f4e0
Michael Gutteridge <mrg@fhcrc.org>
2016-05-03 10:50:00 -0700

    checked in mr. skeleton

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name 'hutch-certs'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures hutch-certs'
+long_description 'Installs/Configures hutch-certs'
+version '0.1.0'
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: hutch-certs
+# Recipe:: default
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
