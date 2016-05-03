cf0d28d565112d2f094e7418b9aa2bdf1b23f763
Michael Gutteridge <mrg@fhcrc.org>
2016-05-03 13:27:43 -0700

    added test writeup

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -3,7 +3,11 @@ require 'spec_helper'
 describe 'hutch-certs::default' do
   # Serverspec examples can be found at
   # http://serverspec.org/resource_types.html
-  it 'does something' do
-    skip 'Replace this with meaningful tests'
+  it 'installs certificates' do
+      # check that dummy certificates are installed
+      # - generate dummy cert
+      # - install using default
+      # - verify presence in cert chain
+      skip 'Replace this with meaningful tests'
   end
 end
