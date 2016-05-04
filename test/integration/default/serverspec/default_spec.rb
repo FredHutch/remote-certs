ec2220d03fc16cec42fcb4d8dcf4639f194a2eb1
Michael Gutteridge <mrg@fhcrc.org>
2016-05-04 14:25:52 -0700

    added check for certificate installation

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -1,13 +1,6 @@
 require 'spec_helper'
 
-describe 'certs::default' do
-  # Serverspec examples can be found at
-  # http://serverspec.org/resource_types.html
-  it 'installs certificates' do
-      # check that dummy certificates are installed
-      # - generate dummy cert
-      # - install using default
-      # - verify presence in cert chain
-      skip 'Replace this with meaningful tests'
-  end
+# Check for presence of certificate in global certs file
+describe command('openssl verify -CAfile /etc/ssl/certs/ca-certificates.crt /var/tmp/sample_certs/sample.crt') do
+    its(:stdout) { should match /OK/ }
 end
