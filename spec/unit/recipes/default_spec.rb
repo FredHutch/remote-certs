eb14c9ee3823b9b985633063b3586ca5a45228f9
Michael Gutteridge <mrg@fhcrc.org>
2016-05-04 14:33:04 -0700

    fixed references to old name

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -1,12 +1,12 @@
 #
-# Cookbook Name:: hutch-certs
+# Cookbook Name:: remote-certs
 # Spec:: default
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
 require 'spec_helper'
 
-describe 'hutch-certs::default' do
+describe 'remote-certs::default' do
   context 'When all attributes are default, on an unspecified platform' do
     let(:chef_run) do
       runner = ChefSpec::ServerRunner.new
