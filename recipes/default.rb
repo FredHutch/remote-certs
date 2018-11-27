68da69b8ac4d58b6ddb83b35291b6050fff5c3af
Michael Gutteridge <michael.gutteridge@gmail.com>
2018-11-27 09:01:11 -0800

    Fix line-length errors from rubocop

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,33 +3,37 @@
 # Recipe:: default
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
-# 
+#
 
 execute 'install_cert' do
-    command node['remote-certs']['update_command']
-    action :nothing
+  command node['remote-certs']['update_command']
+  action :nothing
 end
 
+tld = '/usr/share/ca-certificates'
+org = node['remote-certs']['organization']
+name = node['remote-certs']['name']
+
 # create organization subdirectory for certificate
-directory "/usr/share/ca-certificates/#{node['remote-certs']['organization']}" do
-    owner 'root'
-    group 'root'
-    mode '0755'
-    action :create
+directory "#{tld}/#{org}" do
+  owner 'root'
+  group 'root'
+  mode '0755'
+  action :create
 end
 
 # Install file into certificates directory
-remote_file "/usr/share/ca-certificates/#{node['remote-certs']['organization']}/#{node['remote-certs']['name']}" do
-    source node['remote-certs']['source']
-    owner 'root'
-    group 'root'
-    mode '0644'
-    action :create
+remote_file "#{tld}/#{org}/#{name}" do
+  source node['remote-certs']['source']
+  owner 'root'
+  group 'root'
+  mode '0644'
+  action :create
 end
 
 # Update the certificates configuration with the new cert
 append_if_no_line 'add certificate to config' do
   path '/etc/ca-certificates.conf'
-  line "#{node['remote-certs']['organization']}\/#{node['remote-certs']['name']}"
+  line "#{org}\/#{name}"
   notifies :run, 'execute[install_cert]', :delayed
 end
