0d5f7158fe0b4a214b0ec63e80f5428b4a1b9bf8
Michael Gutteridge <michael.gutteridge@gmail.com>
2018-11-27 09:20:31 -0800

    Rubocop fixes for spec tests

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -1,6 +1,12 @@
 require 'spec_helper'
 
 # Check for presence of certificate in global certs file
-describe command('openssl verify -CAfile /etc/ssl/certs/ca-certificates.crt /var/tmp/sample_certs/cert.pem') do
-    its(:stdout) { should not match /error/ }
+describe command(
+  %w(
+    openssl verify
+    -CAfile /etc/ssl/certs/ca-certificates.crt
+    /var/tmp/sample_certs/cert.pem
+  )
+) do
+  its(:stdout) { should_not match(/error/) }
 end
