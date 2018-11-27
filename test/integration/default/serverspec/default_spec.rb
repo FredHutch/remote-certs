24b50cf95c989c6d3e137fd8812a3e124cde9750
Michael Gutteridge <michael.gutteridge@gmail.com>
2018-11-27 08:40:24 -0800

    Change spec tests
     - update certificate name
     - check for error messages instead of "OK"

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
 # Check for presence of certificate in global certs file
-describe command('openssl verify -CAfile /etc/ssl/certs/ca-certificates.crt /var/tmp/sample_certs/sample.crt') do
-    its(:stdout) { should match /OK/ }
+describe command('openssl verify -CAfile /etc/ssl/certs/ca-certificates.crt /var/tmp/sample_certs/cert.pem') do
+    its(:stdout) { should not match /error/ }
 end
