cebcf4761021991d475b9cffc596c7dbe8d48083
Michael Gutteridge <michael.gutteridge@gmail.com>
2018-11-27 08:47:38 -0800

    Move attributes to readme
     - no longer supported/used
     - c.f. FC072

--- metadata.rb
+++ metadata.rb
@@ -4,33 +4,6 @@ maintainer_email 'scicomp@fhcrc.org'
 license 'all_rights'
 description 'Installs Certificates'
 long_description 'Installs Certificates from HTTP Server for use by OpenSSL'
-version '0.1.3'
+version '0.1.4'
 
 depends 'line', '~> 2.1.1'
-
-attribute 'organization',
-    :display_name => 'Organization Name',
-    :description  => 'The name of the organization that owns the certificate',
-    :type         => 'string',
-    :required     => 'required',
-    :default      => 'organization'
-
-attribute 'source',
-    :display_name => 'URL containing source',
-    :description  => 'URL for the certificate source',
-    :type         => 'string',
-    :required     => 'required'
-
-attribute 'name',
-    :display_name => 'Certificicate Name',
-    :description  => 'Name for the certificate on the host',
-    :type         => 'string',
-    :required     => 'required'
-
-attribute 'update_command',
-    :display_name => 'CA Update Command',
-    :description  => 'Command used to update CA chain',
-    :type         => 'string',
-    :required     => 'required',
-    :default      => '/usr/sbin/update-ca-certificates -f'
-
